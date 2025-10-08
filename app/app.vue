309816a327ed588c956e9a836244f47fb850b018
JSkahl <josekahl333@gmail.com>
2025-10-08 15:24:09 -0300

    chore: cleaning project

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,22 @@
+<script setup>
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+      <h1>Under construction</h1>
+      <p>Please wait...</p>
   </div>
 </template>
+
+<style scoped>
+div {
+    width: 100vw;
+    height: 100vh;
+    background-color: #1e1e2e;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: whitesmoke;
+}
+</style>
